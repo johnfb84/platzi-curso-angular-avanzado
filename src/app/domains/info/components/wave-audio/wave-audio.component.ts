1639f1fb9f7e973e8652688942c413a03214979d
jf <jf@tets.com>
2026-06-14 00:25:09 -0500

    using signal in DOM

--- src/app/domains/info/components/wave-audio/wave-audio.component.ts
+++ src/app/domains/info/components/wave-audio/wave-audio.component.ts
@@ -4,7 +4,8 @@ import {
   ViewChild,
   signal,
   AfterViewInit,
-  input
+  input,
+  viewChild
 } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
@@ -17,14 +18,14 @@ import WaveSurfer from 'wavesurfer.js';
 })
 export class WaveAudioComponent implements AfterViewInit {
   readonly audioUrl = input.required<string>();
-  @ViewChild('wave') container!: ElementRef;
+  $waveContainerRef = viewChild.required<ElementRef<HTMLDivElement>>('wave');
   private ws!: WaveSurfer;
   isPlaying = signal(false);
 
   ngAfterViewInit() {
     this.ws = WaveSurfer.create({
       url: this.audioUrl(),
-      container: this.container.nativeElement,
+      container: this.$waveContainerRef()?.nativeElement,
     });
     this.ws.on('play', () => this.isPlaying.set(true));
     this.ws.on('pause', () => this.isPlaying.set(false));
